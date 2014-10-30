098ea980d2cafb1b6f0b136a4ccd73e7a2d1c777
Amit <peaceamit@gmail.com>
2014-10-30 23:46:57 +0530

    Initial commit

---
+++ app/scripts/routes/player.js
@@ -0,0 +1,16 @@
+/*global define*/
+
+define([
+    'jquery',
+    'backbone'
+], function ($, Backbone) {
+    'use strict';
+
+    var PlayerRouter = Backbone.Router.extend({
+        routes: {
+        }
+
+    });
+
+    return PlayerRouter;
+});
